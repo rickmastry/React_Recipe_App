570fdc4f5093d77d88703c279225cb3b3d923766
jah rik <jahrik@hotmail.com>
2019-07-04 22:41:03 -0400

    Updates

---
+++ precache-manifest.c73240ece0d9359be91d01affac409e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e81fd288ac6abbef9ac4f26227cb9d0",
+    "url": "/React_Recipe_App/index.html"
+  },
+  {
+    "revision": "2731d86c335548d8ea7f",
+    "url": "/React_Recipe_App/static/css/main.285b0012.chunk.css"
+  },
+  {
+    "revision": "ddd4e623390ec3facaf3",
+    "url": "/React_Recipe_App/static/js/2.77b90360.chunk.js"
+  },
+  {
+    "revision": "2731d86c335548d8ea7f",
+    "url": "/React_Recipe_App/static/js/main.3fa68d1f.chunk.js"
+  },
+  {
+    "revision": "b92360fced826ca658c9",
+    "url": "/React_Recipe_App/static/js/runtime~main.779e9f56.js"
+  }
+]);
