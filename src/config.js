976d1e2531e6a959bbed577deb3c59c21324ec6b
jah rik <jahrik@hotmail.com>
2019-07-04 22:23:20 -0400

    first recipe commit

---
+++ src/config.js
@@ -0,0 +1,5 @@
+export const proxy = 'https://cors-anywhere.herokuapp.com/';
+export const key = '03cbff2c53d90523683ebf31581953d6';
+
+//03cbff2c53d90523683ebf31581953d6;
+//cc066a48599ff5cd3c3c06699f0cf0c3
